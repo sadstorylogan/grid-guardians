747514c7e0e4aae07753c963749e7be32ae1db1a
Mehmet Sezgin <44436956+sadstorylogan@users.noreply.github.com>
2023-07-24 12:25:11 +0300

    Added unit walking animations

---
+++ Assets/Scripts/UnitActionSystem.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitActionSystem : MonoBehaviour
+{
+    public event EventHandler OnSelectedUnitChanged;
+
+    [SerializeField] private Unit selectedUnit;
+    [SerializeField] private LayerMask unitLayerMask;
+
+    private void Update()
+    {
+        if (TryHandleUnitSelection()) return;
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            selectedUnit.Move(MouseWorld.GetPosition());
+        }
+    }
+
+    private bool TryHandleUnitSelection()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, unitLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
+            {
+                SetSelectedUnit(unit);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void SetSelectedUnit(Unit unit )
+    {
+        selectedUnit = unit;
+
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public Unit GetSelectedUnit() 
+    { 
+        return selectedUnit;
+    }
+        
+}
